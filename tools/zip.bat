2189e4ab02f8e8c892f7b40dcaed14be7a026e04
Luca Carella <bkarelb@hotmail.it>
2016-11-22 13:11:37 +0100

    removed unused file

--- tools/zip.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"C:\Program Files\7-Zip\7z.exe" u -t7z -r "../build/x86/Release/GLFunBind v1.1 win32.7z" "../build/x86/Release/*.exe"  "../build/x86/Release/include/" "../README" "../gpl-3.0.txt"
